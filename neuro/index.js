a4a977a3ff4efbe0dd921381885b9f31e07e50eb
Angelo <angelo1236540@gmail.com>
2024-09-21 17:06:09 -0500

    Arracar servidor
    Se activo NODE.JS con el servidor y ahora funciona el servidor node.js

--- neuro/index.js
+++ neuro/index.js
@@ -1,6 +1,12 @@
 
 //Importar la libreia de MYSQL
+//importar la libreria de express, http app
 var mysql = require('mysql');
+const http = require('http');
+const express = require('express');
+const app = express();
+
+
 //Configurar la conexion a la base de datos, 
 //en caso de tener una contraseña diferente, 
 //se debe agregar el campo password
@@ -20,19 +26,15 @@ connection.end();
 
 
 
-//importar la libreria de express, http app
-//const http = require('http');
-//const express = require('express');
-//const app = express();
-//Recursos
-//app.use(express.static(__dirname + '/'));
+
+app.use(express.static(__dirname + '/'));
 //Enrutamiento
-//app.get('/', (req, res) => {
-//res.sendFile(__dirname + '../neuro/index.');
-//});
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '../neuro/index.html');
+});
 //Escuchar en el puerto 3000
-//app.listen(3000);
-//console.log('Servidor en el puerto 3000');//
+app.listen(3000);
+console.log('Servidor en el puerto 3000');
 
 
 
