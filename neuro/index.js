67cb809a23b0410701b555f8714d4aea268ecbf3
Angelo <angelo1236540@gmail.com>
2024-09-21 18:03:49 -0500

    NODE.JS
    Aqui si esta corriendo el node.js, veremos que pasa deespues xD

--- neuro/index.js
+++ neuro/index.js
@@ -1,28 +1,30 @@
 // Importar la librería de MYSQL
-//var mysql = require('mysql');
-//const http = require('http');
-//const express = require('express');
-//const app = express();
+var mysql = require('mysql');
+const http = require('http');
+const express = require('express');
+const app = express();
 
 // Configurar la conexión a la base de datos
-//var connection = mysql.createConnection({
-  //  host: 'localhost',
-    //user: 'root',
-   // password: 'root',
-    //database: 'db_n'
-//});
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'db_n'
+});
 
 // Verificar la conexión a la base de datos
 //connection.connect(function (error) {
-  //  if (error) throw error;
-    //console.log('Conexión exitosa a la base de datos');
-//});
-//app.use(express.static(__dirname + '/'));
+connection.connect(function (error) {
+    if (error) throw error;
+    console.log('Conexión exitosa a la base de datos');
+});
+
+app.use(express.static(__dirname + '/'));
 // Enrutamiento
-//app.get('/', (req, res) => {
-  //  res.sendFile(__dirname + '/neuro/index.html');
-//});
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/neuro/index.html');
+});
 // Escuchar en el puerto 3000
-//app.listen(3000, () => {
-  //  console.log('Servidor en el puerto 3000');
-//});
+app.listen(3000, () => {
+    console.log('Servidor en el puerto 3000');
+});
