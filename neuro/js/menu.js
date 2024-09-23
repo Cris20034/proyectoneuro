4544a6ba813b2445c5ae214b9f7f623683b17dbb
Cris20034 <134569737+Cris20034@users.noreply.github.com>
2024-09-22 20:39:16 -0500

    cambios

--- neuro/js/menu.js
+++ neuro/js/menu.js
@@ -13,42 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-// Script para mostrar los detalles de los servicios y el calendario
-const botonesVerMas = document.querySelectorAll('.ver-mas');
 
-botonesVerMas.forEach(boton => {
-  boton.addEventListener('click', () => {
-      const detalles = boton.parentNode.querySelector('.detalles');
-      detalles.style.display = 'block';
 
-      // Ocultar otros detalles (opcional)
-      const otrosDetalles = document.querySelectorAll('.detalles:not(.show)');
-      otrosDetalles.forEach(detalle => {
-          detalle.style.display = 'none';
-      });
-
-      // Inicializar el calendario (si aún no está inicializado)
-      const calendarEl = detalles.querySelector('#calendar');
-      if (!calendarEl.calendar) {
-          const calendar = new FullCalendar.Calendar(calendarEl, {
-              // Configuración del calendario
-              initialView: 'dayGridMonth',
-              selectable: true,
-              headerToolbar: {
-                  left: 'prev,next today',
-                  center: 'title',
-                  right: 'dayGridMonth,timeGridWeek,timeGridDay'
-              },
-              events: [
-                  // Aquí puedes añadir eventos de ejemplo
-                  { title: 'Clase de Yoga', start: '2024-09-25' },
-                  { title: 'Masajes Relajantes', start: '2024-09-30' }
-              ]
-          });
-          calendar.render();
-          calendarEl.calendar = calendar;  // Guarda la instancia del calendario
-      }
-  });
-});
 
 
