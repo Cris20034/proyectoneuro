e2e902597f33faf57792165370501af844f83e2a
Angelo <angelo1236540@gmail.com>
2024-10-01 21:52:26 -0500

    Actulizacion Mas Cosas
    Mas cosas para la app

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect, url_for, session ,make_response
 import psycopg2, re 
 from werkzeug.security import generate_password_hash, check_password_hash
