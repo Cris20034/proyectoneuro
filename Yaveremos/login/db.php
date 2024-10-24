b34e284d4c6f9e549e83c2eaba6739d7eb4891d8
Angelo Benavides <134516530+AngeloAlexanderBenavides@users.noreply.github.com>
2024-10-23 22:07:15 -0500

    Ubicacion
    Cambio de sito algunas cosas

--- Yaveremos/login/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conexion=mysqli_connect("localhost","root","","db_neuro");
-
-
-?>
-<H1>CONECTADO</H1>
--- Yaveremos/login/modelo/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conexion = new mysqli("localhost", "root", "", "bd_neuro");
-$conexion->set_charset("utf8");
-
-?>
-
